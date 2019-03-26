808b0d956770fe031d5deefd9bf2a1d841cbbd48
Barnabas Jovanovics <jovanovics@lovelysystems.com>
2019-03-26 13:56:52 +0100

    udpate gradle and make java 9 compatible

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.Coroutines
 plugins {
     application
     kotlin("jvm") version "1.2.41"
-    id("com.lovelysystems.gradle") version ("0.0.6")
+    id("com.lovelysystems.gradle") version ("1.2.0")
 }
 
 lovely {
@@ -43,6 +43,7 @@ dependencies {
     compile("io.ktor:ktor-server-netty:$ktorVersion")
     compile("ch.qos.logback:logback-classic:1.2.1")
     compile("com.google.guava:guava:27.1-jre")
+    compile("com.sun.xml.ws:jaxws-ri:2.3.2")
     testCompile("org.amshove.kluent:kluent:1.32")
     testCompile("junit:junit:4.12")
     testCompile("io.ktor:ktor-server-test-host:$ktorVersion")
